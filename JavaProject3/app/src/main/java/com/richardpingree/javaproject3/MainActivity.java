e858e971301e97923cc5047813c14b279d4dd7a6
richpingree <richpingree@fullsail.edu>
2014-12-16 08:17:01 -0600

    setup Asynctask on background  and tested api data
    setup Asynctask on background and tested api data

--- JavaProject3/app/src/main/java/com/richardpingree/javaproject3/MainActivity.java
+++ JavaProject3/app/src/main/java/com/richardpingree/javaproject3/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.nfc.Tag;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -13,6 +14,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
@@ -49,17 +57,69 @@ public class MainActivity extends Activity implements View.OnClickListener {
         String getInput = userInput.getText().toString();
 
         getInput = getInput.replaceAll(" ", "_").toLowerCase();
-        userInput.setText("");
+        //Log.i("Test", getInput);
+        try {
+            String baseURL = "http://api.artistlink.com/home/accounts.json?auth_token=5xVzCSGTz4yNaaxyJbcs";
+            URL queryURl = new URL(baseURL + "&name=" + getInput);
+            new myTask().execute(queryURl);
 
-        Log.i("Test", getInput);
+        }  catch (MalformedURLException e) {
+            e.printStackTrace();
+        } {
+
+        }
 
         if (isOnline()){
             Log.i("Test", "you have an internet connection.");
         } else
             Log.i("Test", "internet not available");
 
+    }
+
+    private class myTask extends AsyncTask<URL, Integer, JSONObject>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected JSONObject doInBackground(URL... urls) {
+
+            String jsonString = "";
+
+            //collect api response
+            for (URL queryURL: urls) {
+                try {
+                    URLConnection conn = queryURL.openConnection();
+                    jsonString = IOUtils.toString(conn.getInputStream());
+                    jsonString = jsonString.replace("[","");
+                    jsonString = jsonString.replace("]","");
+                    break;
+                } catch (Exception e) {
+                    Log.e("Test", "could not connect to network" + queryURL.toString());
+                }
+            }
+            Log.i("Test", "recieved data" + jsonString);
 
 
+            //api string to json
 
+            JSONObject apiData;
+
+            try {
+                apiData = new JSONObject(jsonString);
+            } catch (Exception e){
+                Log.e("Test", "can not convert");
+                apiData = null;
+            }
+
+            return apiData;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject apiData) {
+
+        }
     }
 }
