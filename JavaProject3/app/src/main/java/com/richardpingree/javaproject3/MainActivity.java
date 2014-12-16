2db5e4769ef8bc7656469afd63a593a965df856f
richpingree <richpingree@fullsail.edu>
2014-12-15 22:56:26 -0600

    setup UI and added network check
     setup UI  and added network check

--- JavaProject3/app/src/main/java/com/richardpingree/javaproject3/MainActivity.java
+++ JavaProject3/app/src/main/java/com/richardpingree/javaproject3/MainActivity.java
@@ -1,39 +1,65 @@
 package com.richardpingree.javaproject3;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+
+    EditText userInput;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.userText);
+
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(this);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    protected boolean isOnline() {
+        ConnectivityManager connMan = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = connMan.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()){
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
             return true;
-        }
 
-        return super.onOptionsItemSelected(item);
+        }else{
+            return false;
+        }
+    }
+    @Override
+    public void onClick(View v) {
+        String getInput = userInput.getText().toString();
+
+        getInput = getInput.replaceAll(" ", "_").toLowerCase();
+        userInput.setText("");
+
+        Log.i("Test", getInput);
+
+        if (isOnline()){
+            Log.i("Test", "you have an internet connection.");
+        } else
+            Log.i("Test", "internet not available");
+
+
+
+
     }
 }
