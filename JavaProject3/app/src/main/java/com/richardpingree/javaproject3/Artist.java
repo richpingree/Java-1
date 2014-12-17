b1b2bd69a88acfd0e0d08c1be003d65da1b3929a
richpingree <richpingree@fullsail.edu>
2014-12-17 10:17:24 -0600

    declared everything in Artist class as public
    declared everything in Artist class as public

--- JavaProject3/app/src/main/java/com/richardpingree/javaproject3/Artist.java
+++ JavaProject3/app/src/main/java/com/richardpingree/javaproject3/Artist.java
@@ -13,83 +13,84 @@ import org.json.JSONObject;
  */
 public class Artist {
 
-    String artistName;
-    String artistGenre;
-    String artistLabel;
-    String artistCountry;
-    String artistCity;
-    String artistState;
+    public String artistName;
+    public String artistGenre;
+    public String artistLabel;
+    public String artistCountry;
+    public String artistCity;
+    public String artistState;
 
-    Artist(){}
+    public Artist (){}
 
-    Artist(String name, String genre, String label, String country, String city, String state){
-        artistName = name;
-        artistGenre = genre;
-        artistLabel = label;
-        artistCountry = country;
-        artistCity = city;
-        artistState = state;
+    public Artist(String name, String genre, String label, String country, String city, String state){
+            artistName = name;
+            artistGenre = genre;
+            artistLabel = label;
+            artistCountry = country;
+            artistCity = city;
+            artistState = state;
 
     }
 
-    Artist(JSONObject artistData){
-        try{
-            artistName = artistData.getString("name");
-            artistGenre = artistData.getString("genre");
-            artistLabel = artistData.getString("label");
-            artistCountry = artistData.getString("country");
-            artistCity = artistData.getString("city");
-            artistState = artistData.getString("state");
-        } catch (Exception e){
+    public Artist(JSONObject artistData){
+            try{
+                artistName = artistData.getString("name");
+                artistGenre = artistData.getString("genre");
+                artistLabel = artistData.getString("label");
+                artistCountry = artistData.getString("country");
+                artistCity = artistData.getString("city");
+                artistState = artistData.getString("state");
+            } catch (Exception e){
 
             //Log.e("Test", "Error occured.");
-        }
+               }
     }
+
     //gets and sets data for object
-    String getArtistName(){
+    public String getArtistName(){
         return artistName;
     }
 
-    void setArtistName(String artistName) {
+    public void setArtistName(String artistName) {
         this.artistName = artistName;
     }
 
-    String getArtistGenre(){
+    public String getArtistGenre(){
         return artistGenre;
     }
 
-    void setArtistGenre(String artistGenre) {
+    public void setArtistGenre(String artistGenre) {
         this.artistGenre = artistGenre;
     }
 
-    String getArtistLabel(){
+    public String getArtistLabel(){
         return artistLabel;
     }
 
-    void setArtistLabel(String artistLabel) {
+    public void setArtistLabel(String artistLabel) {
         this.artistLabel = artistLabel;
     }
 
-    String getArtistCountry(){
+    public String getArtistCountry(){
         return artistCountry;
     }
 
-    void setArtistCountry(String artistCountry){
+    public void setArtistCountry(String artistCountry){
         this.artistCountry = artistCountry;
     }
 
-    String getArtistCity(){
+    public String getArtistCity(){
         return artistCity;
     }
 
-    void setArtistCity(String artistCity){
+    public void setArtistCity(String artistCity){
         this.artistCity = artistCity;
     }
     String getArtistState(){
         return artistState;
     }
 
-    void setArtistState(String artistState){
+    public void setArtistState(String artistState){
         this.artistState = artistState;
     }
 
